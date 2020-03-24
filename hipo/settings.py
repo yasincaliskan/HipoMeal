7cc7efbfd62aff5bdd9d15eb835226b219683435
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2020-03-24 15:02:27 +0300

    Update static_root

--- hipo/settings.py
+++ hipo/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 ]
 
 # static dosyalar için sunucuya atarken;
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
